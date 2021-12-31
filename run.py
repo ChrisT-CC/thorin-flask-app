723bc043987f6444ac501610290132eba924ad71
ChrisT-CC <cristaecc@gmail.com>
2021-12-31 11:20:55 +0000

    Create HTML templates and link them by routing

--- run.py
+++ run.py
@@ -1,16 +1,28 @@
 import os
 # import the Flask class
-from flask import Flask
+from flask import Flask, render_template
 
 
 # Create an instance of Flask class
 app = Flask(__name__)
 
 
-# use the route decorator to tell Flask what URL should trigger the function that follows
+# use the route decorator to tell Flask what URL should trigger the function
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+# link about page by routing
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# link contact page by routing
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
