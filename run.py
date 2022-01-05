5eb523d307374b1d5e82081866b5d748704f4d62
ChrisT-CC <cristaecc@gmail.com>
2022-01-05 22:35:54 +0000

    Modify pages titles as arguments in render_template function

--- run.py
+++ run.py
@@ -16,19 +16,19 @@ def index():
 # link about page by routing
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 # link contact page by routing
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 # link careers page by routing
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers" )
 
 
 if __name__ == "__main__":
